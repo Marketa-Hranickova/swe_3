188ccb99cac681f94fe84eb8de6a795ed1ec3ee4
maky <hranickovamarketa@gmail.com>
2025-11-13 16:41:03 +0100

    Exercise 6 - creating BeforeEach method and removing duplicated setup

--- src/test/java/at/aau/serg/exercises/math/MyMathTest.java
+++ src/test/java/at/aau/serg/exercises/math/MyMathTest.java
@@ -1,5 +1,6 @@
 package at.aau.serg.exercises.math;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,8 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyMathTest {
 
-    @Test
+    private MyMath mm;
 
+    @BeforeEach
+    public void setUp() {
+        mm = new MyMath();
+    }
+    @Test
     public void testAdd() {
         MyMath mm = new MyMath();
         Double result = mm.add(10d, 5d);
@@ -41,30 +47,25 @@ public class MyMathTest {
     @Test
     public void x2() {
         Fraction f = new Fraction(1,1);
-        MyMath mm = new MyMath();
         Fraction reduced = mm.reduce(f);
         assertEquals(new Integer(1),reduced.getNumerator());
         assertEquals(new Integer(1),reduced.getDenumerator());
 
         f = new Fraction(10,6);
-        mm = new MyMath();
         reduced = mm.reduce(f);
         assertEquals(new Integer(5),reduced.getNumerator());
         assertEquals(new Integer(3),reduced.getDenumerator());
 
         f = new Fraction(10,5);
-        mm = new MyMath();
         reduced = mm.reduce(f);
         assertEquals(new Integer(2),reduced.getNumerator());
         assertEquals(new Integer(1),reduced.getDenumerator());
 
         f = new Fraction(10,5);
-        mm = new MyMath();
         Double aDouble = mm.toDouble(f);
         assertEquals(new Double(2),aDouble);
 
         f = new Fraction(10,4);
-        mm = new MyMath();
         aDouble = mm.toDouble(f);
         assertEquals(new Double(2.5d),aDouble);
     }
